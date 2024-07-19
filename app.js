30d9bb59a7d0df2945bc25c49640f3e058b1789d
Thaynan-Ferreira <thaynan_ferreira@outlook.com.br>
2024-07-19 08:05:47 -0300

    Pegando os valores digitados

---
+++ app.js
@@ -0,0 +1,4 @@
+//pega os valores digitados e os adiciona em variaveis
+let quantNum = document.querySelector('#quantidade').value;
+let inicio = document.querySelector('#de').value;
+let fim = document.querySelector('#ate').value;
