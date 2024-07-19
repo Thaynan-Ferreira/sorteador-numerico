c0726a132ad8762fdc023dfbf234fe3329c94b88
Thaynan-Ferreira <thaynan_ferreira@outlook.com.br>
2024-07-19 08:26:19 -0300

    Inicio da função sortear + recebendo inteiro do HTML

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 //pega os valores digitados e os adiciona em variaveis
-let quantNum = document.querySelector('#quantidade').value;
-let inicio = document.querySelector('#de').value;
-let fim = document.querySelector('#ate').value;
+let quantNum = parseInt(document.querySelector('#quantidade').value);
+let inicio = parseInt(document.querySelector('#de').value);
+let fim = parseInt(document.querySelector('#ate').value);
+let numSorteados = [];
+let numSorteado
+
+//função que determina o sorteio dos numeros de acordo com os paramentros
+function sortear(){
+    for(i = 1; i <= quantNum; i++){
+        //formula que sorteia um numero dentro do intervalo especificado
+        numSorteado = Math.floor(Math.random() * (fim - inicio + 1)) + inicio;
+        console.log(numSorteado);
+        numSorteados.push(numSorteado);
+        numSorteado = 0;
+    }
+    console.log(numSorteados)
+}
