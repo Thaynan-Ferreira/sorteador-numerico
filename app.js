49d060092a7455cc7d86241d9ad9aca6f4e3169c
Thaynan-Ferreira <thaynan_ferreira@outlook.com.br>
2024-07-21 10:26:41 -0300

    Adição de controle de erro de intervalos invalidos

--- app.js
+++ app.js
@@ -8,11 +8,19 @@ let numSorteado
 //função que determina o sorteio dos numeros de acordo com os paramentros
 function sortear(){
     for(i = 1; i <= quantNum; i++){
-        //formula que sorteia um numero dentro do intervalo especificado
-        numSorteado = Math.floor(Math.random() * (fim - inicio + 1)) + inicio;
-        console.log(numSorteado);
-        numSorteados.push(numSorteado);
-        numSorteado = 0;
+
+        if (inicio < fim){
+            //formula que sorteia um numero dentro do intervalo especificado
+            numSorteado = Math.floor(Math.random() * (fim - inicio + 1)) + inicio;
+
+            console.log(numSorteado);
+            numSorteados.push(numSorteado);
+            numSorteado = 0;
+        }
+        else{
+            console.log('Intervalo invalido')
+        }
+        
     }
     console.log(numSorteados)
 }
