ccddb262e4b820ae8cacbef1b217cd63e055fd4e
Thaynan-Ferreira <thaynan_ferreira@outlook.com.br>
2024-07-22 08:32:33 -0300

    Editando o botão reiniciar

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
+//Variavel para alterar o Botão reiniciar
+let reinicio = document.querySelector('#btn-reiniciar')
+
+//Variavel para mandar o resultado para o Front
+let resultado = document.querySelector('#resultado')
+
+//função que limpa os campos e desativa novamente o botão de reinicio
 function limpaCampo(){
     document.querySelector('#quantidade').value = '';
     document.querySelector('#de').value = '';
     document.querySelector('#ate').value = '';
+
+    resultado.innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>'
+
+    reinicio.style.cssText = 'background: #6f6f70;' + 'cursor: not-allowed;'
 }
 
 //função que determina o sorteio dos numeros de acordo com os paramentros
@@ -15,9 +26,6 @@ function sortear(){
     //variaveis necessarias para guardar os numeros sorteados
     let numSorteados = [];
     let numSorteado
-
-    //Variavel para mandar o resultado para o Front
-    let resultado = document.querySelector('#resultado')
 
     //for que percorre a quantidade de numeros a sere sorteados
     for(i = 1; i <= quantNum; i++){
@@ -35,7 +43,16 @@ function sortear(){
         }
         
     }
+    //exibe o resultado na tela
     resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${numSorteados} </label>`
 
+    //ativa o botão reiniciar 
+    reinicio.style.cssText = 'background: #1875E8;' + 'cursor: pointer;'
+
+}
+
+//função que reinicia o jogo
+function reiniciar() {
     limpaCampo();
+    
 }
